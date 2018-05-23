68085ff62468bb6fdac2b94699d8433a4806102e
AntonLaptop <texfan@gmail.com>
2018-05-23 11:55:35 -0500

    hype

--- solidity/SquareHype.sol
+++ solidity/SquareHype.sol
@@ -1,13 +1,24 @@
 pragma solidity ^0.4.0;
 
 contract SquareHype {
-    string storedPotato;
+  struct Pixel {
+    uint8[3] rgb;
+  }
 
-    function setPotato(string x) public {
-      storedPotato = x;
-    }
+  uint constant width = 4;
+  uint constant height = 4;
+  
+  Pixel[width*height] screen;
 
-    function getPotato() public view returns (string) {
-      return storedPotato;      
-    }
+  function getIndex(uint8 x, uint8 y) public pure returns(uint){
+    return y*width+x;
+  }
+
+  function getColor(uint8 x, uint8 y) public view returns(uint8[3]){
+    return screen[getIndex(x,y)].rgb;
+  }
+  
+  function setColor(uint8 x, uint8 y, uint8[3] clr) public{
+    screen[getIndex(x,y)].rgb = clr;
+  }
 }
