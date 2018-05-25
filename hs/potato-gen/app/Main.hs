824339dc24796d7c9387766bcaee6e4c1c34adf2
peter lu <chippermonky@gmail.com>
2018-05-25 13:20:34 -0700

    disabled gloss

--- hs/potato-gen/app/Main.hs
+++ hs/potato-gen/app/Main.hs
@@ -14,18 +14,13 @@ import Lib
 
 main = do
     web3 <- query
-    -- ie <- readImageRGBA "1024.jpg"
     let
         wi = either (\e -> trace (show e) undefined) id web3
-        --i = either error id ie
         arr :: R.Array R.U R.DIM3 Word8
         arr = R.fromList (Z:.bwidth:.bheight:.3) wi
         wimg = Img $ R.delay arr
-        -- mkPic img = let (Z:.c:.r:.z) = extent (imgData img)
-        --     in bitmapOfByteString r c (BitmapFormat TopToBottom PxRGBA) (toByteString img) True
+        mkPic img = let (Z:.c:.r:.z) = extent (imgData img)
+             in bitmapOfByteString r c (BitmapFormat TopToBottom PxRGBA) (toByteString img) True
         glossImg =  (mkPic $ wimg)
     B.writeFile ("output.png") $ imageToPng (imgToImage wimg)
-    display (InWindow "mochi" (800,600) (0,0)) white (Scale 100 100 glossImg)
-    -- display (FullScreen (800,600)) white (mkPic i)
-    -- display (FullScreen (800,600)) white (mkPic $ flipHorizontally`onImg`  i)
-    -- display (FullScreen (800,600)) white (mkPic $ (flipVertically . flipHorizontally) `onImg` i)
+    -- display (InWindow "mochi" (800,600) (0,0)) white (Scale 100 100 glossImg)
